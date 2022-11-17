d5b9ddd02189b7280aba94840fce9c07c508193c
Asmit Gautam <mailtoasmitgautam@gmail.com>
2022-11-17 18:49:31 +0545

    design modifications

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Next.js + Tailwind CSS Example
+# Next.js-13 + Tailwind CSS Example
 This is the code version of a tour home page design on dribbble by Masud Rana
 Link to the design >>> https://dribbble.com/shots/17340256-Landing-Page-Travel-Tour-Trip
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="max-w-4xl mx-auto grid grid-cols-2">
-      <div className='mt-20 p-8'>
+    <div className="max-w-screen-2xl mx-auto grid grid-cols-4">
+      <div className='mt-48 p-8 col-span-2'>
         <h1 className='text-[64px] font-extrabold text-gray-800 leading-none'>Explore a new World.</h1>
         <h3 className='text-[18px] text-gray-400 mt-10'>No matter where in the world you want to go, we can help you get there</h3>
         <button className='text-indigo-700 rounded-full bg-white px-6 p-2 outline outline-offset-2 outline-1 mt-10'>Explore Now</button>
-        <div className='flex space-x-2 mt-10'>          
+        <div className='flex space-x-2 mt-52'>          
         <Image src="/trekker.jpeg" alt="trekker logo" width={50} height={50} />
           <div>
             <h3 className='text-md font-semibold'>1000 + Hiking</h3>
@@ -16,7 +16,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className='mt-20'>
+      <div className='mt-20 col-span-2'>
         <Image src="/banner.png" alt="banner image" width={800} height={800} />
       </div>
     </div>
